29c904f7bed74a0896dd486adbeb6746857e9dd5
leandr0 <leandro1604@gmail.com>
2020-01-30 17:44:17 -0300

    Updade Producer to work with the qualifiers for mongodb session collection

--- rotulagem/src/main/java/com/lrgoncalves/registroin/rotulagem/data/Producer.java
+++ rotulagem/src/main/java/com/lrgoncalves/registroin/rotulagem/data/Producer.java
@@ -4,23 +4,67 @@ import java.util.logging.Logger;
 
 import javax.enterprise.inject.Produces;
 
+import org.bson.Document;
+
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 /**
  * @author digitallam
  *
  */
 public class Producer {
-	
+
 	private static final Logger LOGGER = Logger.getLogger(Producer.class.getName());
-	
-	@Produces
-	public MongoClient mongoClient() {
+
+	private static MongoClient mongoClient;
+
+	private static MongoDatabase database;
+
+	private static MongoCollection<Document> clientCollection;
+
+	private static MongoCollection<Document> rotuloCollection;
+
+	private MongoClient clientConnect() {
 		try {
-			return new MongoClient("localhost", 27017);
+
+			if (mongoClient == null)
+				mongoClient = new MongoClient("localhost", 27017);
+
+			return mongoClient;
+
 		} catch (Throwable t) {
 			LOGGER.severe(t.getMessage());
 		}
 		return null;
+	}
+
+	private MongoDatabase databaseConnect() {
+
+		if (database == null)
+			database = clientConnect().getDatabase("registroin");
+
+		return database;
+	}
+
+	@Produces 
+	@ClientCollection
+	public MongoCollection<Document> clientCollection() {
+
+		if (clientCollection == null)
+			clientCollection = databaseConnect().getCollection("clients");
+
+		return clientCollection;
+	}
+
+	@Produces 
+	@RotuloCollection
+	public MongoCollection<Document> rotuloCollection() {
+
+		if (rotuloCollection == null)
+			rotuloCollection = databaseConnect().getCollection("rotulos");
+
+		return rotuloCollection;
 	}
 }
