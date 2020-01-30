06beb794f3c8a7cae2c751e8ddfb6bc39ecbbc0e
leandr0 <leandro1604@gmail.com>
2020-01-30 17:41:43 -0300

    Qualifiers Annotations for CDI injectios regarding MondoDB collections

---
+++ rotulagem/src/main/java/com/lrgoncalves/registroin/rotulagem/data/ClientCollection.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.lrgoncalves.registroin.rotulagem.data;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+/**
+ * @author digitallam
+ *
+ */
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.FIELD})
+public @interface ClientCollection {
+
+}
